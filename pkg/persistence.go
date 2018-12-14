2601e23e1f0295d997d98608e11cde724314685c
Dr. Adedayo Adetoye <dayo.dev@gmail.com>
2018-12-14 11:58:12 +0000

    Refactorings

--- pkg/persistence.go
+++ pkg/persistence.go
@@ -1,4 +1,4 @@
-package websocket
+package tlsaudit
 
 import (
 	"bytes"
@@ -6,161 +6,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"math/rand"
-	"net/http"
 	"os"
-	"sort"
+	"path/filepath"
 	"strings"
 	"time"
-	"path/filepath"
 
-	"github.com/adedayo/cidr"
-	"github.com/dgraph-io/badger"
-
-	"github.com/adedayo/tlsaudit/pkg"
 	"github.com/adedayo/tlsaudit/pkg/model"
-
-	"github.com/gorilla/websocket"
+	"github.com/dgraph-io/badger"
 )
 
 var (
-	allowedOrigins = []string{
-		"auditmate.local:12345",
-	}
-
-	upgrader = websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-		CheckOrigin: func(r *http.Request) bool {
-			for _, origin := range allowedOrigins {
-				if origin == r.Host {
-					return true
-				}
-			}
-			return false
-		},
-	}
-	// dbCatalogueDirectory = "data/tlsaudit/catalogue"
 	dayFormat           = "2006-01-02"
-	baseScanDBDirectory =  filepath.FromSlash("data/tlsaudit/scan")
-	// scanDbDirectory     = filepath.Join(baseScanDBDirectory, time.Now().Format(dayFormat)) 
+	baseScanDBDirectory = filepath.FromSlash("data/tlsaudit/scan")
 )
-
-type mydata struct {
-	Name string
-	Data []byte
-}
-
-//RealtimeScan runs a scan asynchronously and streams result over a websocket
-func RealtimeScan(w http.ResponseWriter, req *http.Request) {
-	if conn, err := upgrader.Upgrade(w, req, nil); err == nil {
-		go func() {
-			var request tlsmodel.ScanRequest
-			if err := conn.ReadJSON(&request); err == nil {
-
-				hosts := []string{}
-				psr := tlsmodel.PersistedScanRequest{}
-				if request.ScanID == "" { //start a fresh scan
-					request.ScanID = GetNextScanID()
-					for _, x := range request.CIDRs {
-						rng := "/32"
-						ports := ""
-						if strings.Contains(x, "/") {
-							rng = "/" + strings.Split(x, "/")[1]
-						}
-						if strings.Contains(x, ":") {
-							ports = strings.Split(strings.Split(x, "/")[0], ":")[1]
-							x = strings.Split(x, ":")[0] + rng
-
-						} else {
-							x = strings.Split(x, "/")[0] + rng
-						}
-						hs := cidr.Expand(x)
-						if ports != "" {
-							for i, h := range hs {
-								hh := strings.Split(h, "/")
-								hs[i] = fmt.Sprintf("%s:%s/%s", hh[0], ports, hh[1])
-							}
-						}
-						hosts = append(hosts, hs...)
-					}
-					//shuffle hosts randomly
-					rand.Shuffle(len(hosts), func(i, j int) {
-						hosts[i], hosts[j] = hosts[j], hosts[i]
-					})
-					psr.Hosts = hosts
-					psr.ScanStart = time.Now()
-					request.Day = psr.ScanStart.Format(dayFormat)
-					psr.Request = request
-					PersistScanRequest(psr)
-				} else {
-					//resume an 
-					//LoadScanRequest retrieves persisted scan request from folder following a layout patternexisting scan
-					psr, err = LoadScanRequest(request.Day,request.ScanID)
-					if err != nil {
-						return
-					}
-				}
-				scanID := psr.Request.ScanID
-
-				//callback function to stream results over a websocket
-				callback := func(position int, results []tlsmodel.ScanResult, narrative string) {
-					// persistScans(fmt.Sprintf("%s;%s:%s", scanID, result.Server, result.Port), result)
-					res := []tlsmodel.HumanScanResult{}
-					for _, r := range results {
-						res = append(res, r.ToStringStruct())
-					}
-					out := tlsmodel.ScanProgress{
-						ScanID:      scanID,
-						Progress:    100 * float32(position) / float32(len(psr.Hosts)),
-						ScanResults: res,
-						Narrative:   narrative,
-					}
-					conn.WriteJSON(out)
-				}
-
-				streamExistingResult(psr,callback)
-				for index, host := range psr.Hosts {
-					if index < psr.Progress {
-						continue
-					}
-					position := index + 1
-					scan := make(map[string]tlsmodel.ScanResult)
-					results := []<-chan tlsmodel.ScanResult{}
-					results = append(results, tlsaudit.ScanCIDRTLS(host, request.Config))
-					for result := range tlsaudit.MergeResultChannels(results...) {
-						key := result.Server + result.Port
-						if _, present := scan[key]; !present {
-							scan[key] = result
-							narrative := fmt.Sprintf("Partial scan of %s. Progress %f%% %d hosts of a total of %d in %f seconds\n",
-								result.Server, 100*float32(position)/float32(len(psr.Hosts)), position, len(psr.Hosts), time.Since(psr.ScanStart).Seconds())
-							callback(position, []tlsmodel.ScanResult{result}, narrative)
-						}
-					}
-					psr.Progress = position
-					psr.ScanEnd = time.Now()
-					PersistScanRequest(psr)
-					var scanResults []tlsmodel.ScanResult
-					for k := range scan {
-						scanResults = append(scanResults, scan[k])
-					}
-					sort.Sort(tlsmodel.ScanResultSorter(scanResults))
-					PersistScans(psr, host, scanResults)
-					narrative := fmt.Sprintf("Finished scan of %s. Progress %f%% %d hosts of a total of %d in %f seconds\n",
-						host, 100*float32(position)/float32(len(psr.Hosts)), position, len(psr.Hosts), psr.ScanEnd.Sub(psr.ScanStart).Seconds())
-					callback(position, scanResults, narrative)
-
-				}
-			} else {
-				println(err.Error())
-				return
-			}
-		}()
-	} else {
-		println(err.Error())
-		return
-	}
-}
 
 //ListScans returns the ScanID list of persisted scans
 func ListScans(rewindDays int) (result []tlsmodel.ScanRequest) {
@@ -173,7 +31,6 @@ func ListScans(rewindDays int) (result []tlsmodel.ScanRequest) {
 		log.Print(err)
 		return
 	}
-
 
 	allowedDates := make(map[string]bool)
 	today := time.Now()
@@ -198,17 +55,34 @@ func ListScans(rewindDays int) (result []tlsmodel.ScanRequest) {
 
 		for _, sID := range dirs {
 			scanID := sID.Name()
-			//LoadScanRequest retrieves persisted scan request from folder following a layout patternme()
-			if psr, err := LoadScanRequest(d,scanID); err == nil {
-				result = append(result,psr.Request) 
+			//LoadScanRequest retrieves persisted scan request from folder following a layout pattern
+			if psr, err := LoadScanRequest(d, scanID); err == nil {
+				result = append(result, psr.Request)
 			}
 		}
 	}
-	
 
 	return
 }
 
+//StreamScan streams the result to a callback function
+func StreamScan(day, scanID string, callback func(progress, total int, results []tlsmodel.HumanScanResult)) {
+	if psr, err := LoadScanRequest(day, scanID); err == nil {
+		tot := psr.Progress
+		streamExistingResult(psr, func(progress int, results []tlsmodel.ScanResult, narrative string) {
+			callback(progress, tot, humanise(results))
+		})
+	}
+}
+
+func humanise(in []tlsmodel.ScanResult) (out []tlsmodel.HumanScanResult) {
+	for _, r := range in {
+		out = append(out, r.ToStringStruct())
+	}
+	return
+}
+
+//StreamExistingResult sends data via a callback function
 func streamExistingResult(psr tlsmodel.PersistedScanRequest,
 	callback func(progress int, result []tlsmodel.ScanResult, narrative string)) {
 	opts := badger.DefaultOptions
@@ -258,9 +132,9 @@ func streamExistingResult(psr tlsmodel.PersistedScanRequest,
 //PersistScans persists the result of scans per server
 func PersistScans(psr tlsmodel.PersistedScanRequest, server string, scans []tlsmodel.ScanResult) {
 	opts := badger.DefaultOptions
-	
 	opts.Dir = filepath.Join(baseScanDBDirectory, psr.Request.Day, psr.Request.ScanID)
 	opts.ValueDir = filepath.Join(baseScanDBDirectory, psr.Request.Day, psr.Request.ScanID)
+	opts.NumVersionsToKeep = 0
 	db, err := badger.Open(opts)
 	if err != nil {
 		log.Fatal(err)
@@ -319,6 +193,7 @@ func PersistScanRequest(psr tlsmodel.PersistedScanRequest) {
 	opts := badger.DefaultOptions
 	opts.Dir = filepath.Join(baseScanDBDirectory, psr.Request.Day, psr.Request.ScanID, "request")
 	opts.ValueDir = filepath.Join(baseScanDBDirectory, psr.Request.Day, psr.Request.ScanID, "request")
+	opts.NumVersionsToKeep = 0
 	db, err := badger.Open(opts)
 	if err != nil {
 		log.Fatal(err)
@@ -329,10 +204,65 @@ func PersistScanRequest(psr tlsmodel.PersistedScanRequest) {
 	db.Update(func(txn *badger.Txn) error {
 		return txn.Set([]byte(psr.Request.ScanID), psr.Marshall())
 	})
+
+	if psr.Progress%10 == 0 { //compact DB every 10 run
+		lsmx, vlogx := db.Size()
+		for db.RunValueLogGC(.8) == nil {
+			lsmy, vlogy := db.Size()
+			println("Compacted DB")
+			fmt.Printf("Before LSM: %d, VLOG: %d, After LSM: %d, VLOG: %d\n", lsmx, vlogx, lsmy, vlogy)
+			lsmx, vlogx = lsmy, vlogy
+		}
+	}
 }
-//GetNextScanID returns the next unique scan ID 
+
+//CompactDB reclaims space by pruning the database
+func CompactDB(dayPath, scanID string) {
+
+	//compact the scan requests
+	opts := badger.DefaultOptions
+	opts.Dir = filepath.Join(baseScanDBDirectory, dayPath, scanID, "request")
+	opts.ValueDir = filepath.Join(baseScanDBDirectory, dayPath, scanID, "request")
+	opts.NumVersionsToKeep = 0
+	db, err := badger.Open(opts)
+	if err != nil {
+		println(err.Error())
+		log.Fatal(err)
+		return
+	}
+	lsmx, vlogx := db.Size()
+	for db.RunValueLogGC(.8) == nil {
+		lsmy, vlogy := db.Size()
+		println("Compacted DB", opts.Dir)
+		fmt.Printf("Before LSM: %d, VLOG: %d, After LSM: %d, VLOG: %d\n", lsmx, vlogx, lsmy, vlogy)
+		lsmx, vlogx = lsmy, vlogy
+	}
+	db.Close()
+
+	//compact the scan results
+	opts.Dir = filepath.Join(baseScanDBDirectory, dayPath, scanID)
+	opts.ValueDir = filepath.Join(baseScanDBDirectory, dayPath, scanID)
+	db, err = badger.Open(opts)
+	if err != nil {
+		println(err.Error())
+
+		log.Fatal(err)
+		return
+	}
+	lsmx, vlogx = db.Size()
+	for db.RunValueLogGC(.8) == nil {
+		lsmy, vlogy := db.Size()
+		println("Compacted DB", opts.Dir)
+		fmt.Printf("Before LSM: %d, VLOG: %d, After LSM: %d, VLOG: %d\n", lsmx, vlogx, lsmy, vlogy)
+		lsmx, vlogx = lsmy, vlogy
+	}
+	db.Close()
+
+}
+
+//GetNextScanID returns the next unique scan ID
 func GetNextScanID() string {
-	prefix := filepath.Join(baseScanDBDirectory, time.Now().Format(dayFormat)) 
+	prefix := filepath.Join(baseScanDBDirectory, time.Now().Format(dayFormat))
 	if _, err := os.Stat(prefix); os.IsNotExist(err) {
 		if err2 := os.MkdirAll(prefix, 0755); err2 != nil {
 			log.Fatal("Could not create the path ", prefix)
---
+++ pkg/websocket.go
@@ -0,0 +1,151 @@
+package tlsaudit
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/adedayo/cidr"
+
+	"github.com/adedayo/tlsaudit/pkg/model"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	allowedOrigins = []string{
+		"auditmate.local:12345",
+	}
+
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			for _, origin := range allowedOrigins {
+				if origin == r.Host {
+					return true
+				}
+			}
+			return false
+		},
+	}
+)
+
+type mydata struct {
+	Name string
+	Data []byte
+}
+
+//RealtimeScan runs a scan asynchronously and streams result over a websocket
+func RealtimeScan(w http.ResponseWriter, req *http.Request) {
+	if conn, err := upgrader.Upgrade(w, req, nil); err == nil {
+		go func() {
+			var request tlsmodel.ScanRequest
+			if err := conn.ReadJSON(&request); err == nil {
+
+				hosts := []string{}
+				psr := tlsmodel.PersistedScanRequest{}
+				if request.ScanID == "" { //start a fresh scan
+					request.ScanID = GetNextScanID()
+					for _, x := range request.CIDRs {
+						rng := "/32"
+						ports := ""
+						if strings.Contains(x, "/") {
+							rng = "/" + strings.Split(x, "/")[1]
+						}
+						if strings.Contains(x, ":") {
+							ports = strings.Split(strings.Split(x, "/")[0], ":")[1]
+							x = strings.Split(x, ":")[0] + rng
+
+						} else {
+							x = strings.Split(x, "/")[0] + rng
+						}
+						hs := cidr.Expand(x)
+						if ports != "" {
+							for i, h := range hs {
+								hh := strings.Split(h, "/")
+								hs[i] = fmt.Sprintf("%s:%s/%s", hh[0], ports, hh[1])
+							}
+						}
+						hosts = append(hosts, hs...)
+					}
+					//shuffle hosts randomly
+					rand.Shuffle(len(hosts), func(i, j int) {
+						hosts[i], hosts[j] = hosts[j], hosts[i]
+					})
+					psr.Hosts = hosts
+					psr.ScanStart = time.Now()
+					request.Day = psr.ScanStart.Format(dayFormat)
+					psr.Request = request
+					PersistScanRequest(psr)
+				} else {
+					//resume an
+					//LoadScanRequest retrieves persisted scan request from folder following a layout patternexisting scan
+					psr, err = LoadScanRequest(request.Day, request.ScanID)
+					if err != nil {
+						return
+					}
+				}
+				scanID := psr.Request.ScanID
+
+				//callback function to stream results over a websocket
+				callback := func(position int, results []tlsmodel.ScanResult, narrative string) {
+					// persistScans(fmt.Sprintf("%s;%s:%s", scanID, result.Server, result.Port), result)
+					res := []tlsmodel.HumanScanResult{}
+					for _, r := range results {
+						res = append(res, r.ToStringStruct())
+					}
+					out := tlsmodel.ScanProgress{
+						ScanID:      scanID,
+						Progress:    100 * float32(position) / float32(len(psr.Hosts)),
+						ScanResults: res,
+						Narrative:   narrative,
+					}
+					conn.WriteJSON(out)
+				}
+
+				streamExistingResult(psr, callback)
+				for index, host := range psr.Hosts {
+					if index < psr.Progress {
+						continue
+					}
+					position := index + 1
+					scan := make(map[string]tlsmodel.ScanResult)
+					results := []<-chan tlsmodel.ScanResult{}
+					results = append(results, ScanCIDRTLS(host, request.Config))
+					for result := range MergeResultChannels(results...) {
+						key := result.Server + result.Port
+						if _, present := scan[key]; !present {
+							scan[key] = result
+							narrative := fmt.Sprintf("Partial scan of %s. Progress %f%% %d hosts of a total of %d in %f seconds\n",
+								result.Server, 100*float32(position)/float32(len(psr.Hosts)), position, len(psr.Hosts), time.Since(psr.ScanStart).Seconds())
+							callback(position, []tlsmodel.ScanResult{result}, narrative)
+						}
+					}
+					psr.Progress = position
+					psr.ScanEnd = time.Now()
+					PersistScanRequest(psr)
+					var scanResults []tlsmodel.ScanResult
+					for k := range scan {
+						scanResults = append(scanResults, scan[k])
+					}
+					sort.Sort(tlsmodel.ScanResultSorter(scanResults))
+					PersistScans(psr, host, scanResults)
+					narrative := fmt.Sprintf("Finished scan of %s. Progress %f%% %d hosts of a total of %d in %f seconds\n",
+						host, 100*float32(position)/float32(len(psr.Hosts)), position, len(psr.Hosts), psr.ScanEnd.Sub(psr.ScanStart).Seconds())
+					callback(position, scanResults, narrative)
+
+				}
+			} else {
+				println(err.Error())
+				return
+			}
+		}()
+	} else {
+		println(err.Error())
+		return
+	}
+}
