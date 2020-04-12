ec80673c4185bc7aca90a2cefef5c4759b6d302f
Dr. Adedayo Adetoye <dayo.dev@gmail.com>
2020-04-12 19:51:03 +0100

    Fix: zero the score if any of protocol, key exchange or cipher encryption score is zero

--- pkg/model/tls_score.go
+++ pkg/model/tls_score.go
@@ -11,6 +11,17 @@ func score2009p(s *ScanResult) (result SecurityScore) {
 	return
 }
 
+func score2009q(s *ScanResult) (result SecurityScore) {
+	if s.SupportsTLS() {
+		result = computeBasicScore(s)
+		adjustScore2009q(&result, *s)
+	} else {
+		//No TLS
+		result.Grade = toTLSGrade(-1)
+	}
+	return
+}
+
 func computeBasicScore(s *ScanResult) (result SecurityScore) {
 	max := uint16(0)
 	min := uint16(1000)
@@ -50,19 +61,13 @@ func computeBasicScore(s *ScanResult) (result SecurityScore) {
 
 	result.CipherEncryptionScore = (cipherStrengthMaxScore + cipherStrengthMinScore) / 2
 
-	result.Grade = toTLSGrade((30*result.ProtocolScore + 30*result.KeyExchangeScore + 40*result.CipherEncryptionScore) / 100)
+	if result.ProtocolScore*result.KeyExchangeScore*result.CipherEncryptionScore == 0 {
+		//if any of the three protocol, key exchange or cipher encryption score is zero, then zero the result
+		result.Grade = toTLSGrade(0)
+	} else {
+		result.Grade = toTLSGrade((30*result.ProtocolScore + 30*result.KeyExchangeScore + 40*result.CipherEncryptionScore) / 100)
+	}
 
 	scoreCertificate(&result, s)
-	return
-}
-
-func score2009q(s *ScanResult) (result SecurityScore) {
-	if s.SupportsTLS() {
-		result = computeBasicScore(s)
-		adjustScore2009q(&result, *s)
-	} else {
-		//No TLS
-		result.Grade = toTLSGrade(-1)
-	}
 	return
 }
